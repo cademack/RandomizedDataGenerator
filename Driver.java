4aff7b5d30123d2a74ab33e9cc79c48c750b803c
Cade Mack <mack.cade1@gmail.com>
2020-01-28 09:59:49 -0500

    Just uploading the files
    I wrote this code several months ago, but I figured I would upload them to GitHub on the off chance that someone can use it.

---
+++ Driver.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.*;
+/**
+ *
+ * @author -------
+ * @version --------
+ */
+public class Driver {
+    public static void main(String args[]) {
+        Randomizer instance = new Randomizer();
+        Map<Integer, Double> numProbs = new HashMap();
+        Map<Integer, Integer> counts = new HashMap();
+        int out = 0;
+        for (int i = 1; i < 100; i ++) {
+            numProbs.put(i, .01);
+            counts.put(i, 0);
+        }
+        
+        
+        int size = 10000;
+        
+        for (int i = 0; i < size; i++) {
+            out = instance.randNum(numProbs);
+            counts.put(out, counts.get(out)+1);
+            System.out.println(out);
+        }
+        
+        ArrayList<Integer> list = instance.getReplayList();
+        
+        System.out.println(list.size());
+        
+        for (Integer key : counts.keySet()) {
+            System.out.println(key + " : " + counts.get(key) + " : " + ((double)counts.get(key) / (double)size));
+        }
+        
+        
+    }
+}
---
+++ Randomizer.java
@@ -0,0 +1,75 @@
+import java.util.*;
+import java.lang.Math;
+/**
+ * Picks a random number based on an associative array of numbers
+ * and respective probabilities
+ * 
+ * @author --------
+ * @version -------
+ */
+public class Randomizer {
+    
+    private ArrayList<Integer> replay;
+    
+    /**
+     * Default constructor, initializes the replay ArrayList
+     */
+    public Randomizer() {
+        replay = new ArrayList();
+    }
+    
+    /**
+     * This constructor provides a parameter to set the initial size of the replay list
+     * @param memorySize 
+     */
+    public Randomizer(int memorySize) {
+        replay = new ArrayList(memorySize);
+    }
+    
+    /**
+     * Generates a random number based on a map of integers to their respective probabilities
+     * @param map The map that connects integers to their probabilities of being outputted
+     * @return the integer that is generated
+     */
+    public int randNum(Map<Integer, Double> map) {
+        double rand = Math.random(); //Random Double value, from 0.0 to 0.99999
+        int out = 0; //Placeholder value
+        /*
+        Assuming that the probabilities in the map add up to 1,
+        then this zero should never be returned.
+        */
+        for (Integer i : map.keySet()) {
+            /*
+            So each probability is subtracted from the random double until it is negative
+            Then the last probability to make it negative is correlated to the number to be returned
+            
+            */
+            out = i;
+            rand -= map.get(i);
+            if (rand <= 0) {
+                //Once it is negative, the loop should stop
+                break;
+            }
+        }
+        //Adding the number to the replay list
+        replay.add(out);
+        
+        //Returning the number
+        return out;
+    }
+    
+    /**
+     * This resets the replay list
+     */
+    public void clearReplayList() {
+        replay.clear();
+    }
+    
+    /**
+     * This returns the list of all the generated numbers
+     * @return the list of generated numbers
+     */
+    public ArrayList<Integer> getReplayList() {
+        return replay;
+    }
+}
